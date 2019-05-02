a212d9be02a4fc5d5ac3aec95c5059c5d6fec967
Akito Ito <akito0107@gmail.com>
2019-05-02 17:06:11 +0900

    feat: impl tokenizer

--- tokenizer.go
+++ tokenizer.go
@@ -1,7 +1,9 @@
 package xsqlparser
 
 import (
+	"fmt"
 	"io"
+	"strings"
 	"text/scanner"
 
 	"github.com/pkg/errors"
@@ -11,9 +13,10 @@ import (
 
 type Token int
 
+//go:generate stringer -type Token
 const (
 	// A keyword (like SELECT)
-	SQLWord Token = iota
+	SQLKeyword Token = iota
 	// Numeric literal
 	Number
 	// A character that cloud not be tokenized
@@ -76,9 +79,55 @@ const (
 	ILLEGAL
 )
 
+type SQLWord struct {
+	Value      string
+	QuoteStyle rune
+	Keyword    string
+}
+
+func (s *SQLWord) String() string {
+	if s.QuoteStyle == '"' || s.QuoteStyle == '[' || s.QuoteStyle == '`' {
+		return fmt.Sprintf("%s%s%s", string(s.QuoteStyle), s.Value, string(matchingEndQuote(s.QuoteStyle)))
+	} else if s.QuoteStyle == 0 {
+		return fmt.Sprintf("%q", s.Value)
+	}
+	return ""
+}
+
+func matchingEndQuote(quoteStyle rune) rune {
+	switch quoteStyle {
+	case '"':
+		return '"'
+	case '[':
+		return ']'
+	case '`':
+		return '`'
+	}
+	return 0
+}
+
+func MakeKeyword(word string, quoteStyle rune) *SQLWord {
+	w := strings.ToUpper(word)
+
+	_, ok := dialect.Keywords[w]
+
+	if quoteStyle == 0 && ok {
+		return &SQLWord{
+			Value:   word,
+			Keyword: w,
+		}
+	} else {
+		return &SQLWord{
+			Value:      word,
+			Keyword:    w,
+			QuoteStyle: quoteStyle,
+		}
+	}
+}
+
 type TokenSet struct {
 	Tok   Token
-	Value string
+	Value interface{}
 	Pos   *TokenPos
 }
 
@@ -94,7 +143,7 @@ type Tokenizer struct {
 	Col     int
 }
 
-func Init(src io.Reader, dialect dialect.Dialect) *Tokenizer {
+func NewTokenizer(src io.Reader, dialect dialect.Dialect) *Tokenizer {
 	var scan scanner.Scanner
 	return &Tokenizer{
 		Dialect: dialect,
@@ -120,6 +169,9 @@ func (t *Tokenizer) Tokenize() ([]*TokenSet, error) {
 
 func (t *Tokenizer) NextToken() (*TokenSet, error) {
 	tok, str, err := t.next()
+	if err == io.EOF {
+		return nil, io.EOF
+	}
 	if err != nil {
 		return &TokenSet{Tok: ILLEGAL, Value: "", Pos: t.Pos()}, errors.Wrap(err, "tokenize failed")
 	}
@@ -134,31 +186,296 @@ func (t *Tokenizer) Pos() *TokenPos {
 	}
 }
 
-func (t *Tokenizer) next() (Token, string, error) {
+func (t *Tokenizer) next() (Token, interface{}, error) {
 	r := t.Scanner.Peek()
-
-	switch r {
-	case ' ':
+	switch {
+	case ' ' == r:
 		t.Scanner.Next()
 		t.Col += 1
 		return Whitespace, " ", nil
-	case '\t':
+
+	case '\t' == r:
 		t.Scanner.Next()
 		t.Col += 4
 		return Whitespace, "\t", nil
-	case '\n':
+
+	case '\n' == r:
 		t.Scanner.Next()
 		t.Line += 1
 		t.Col = 1
-		return Whitespace, "\t", nil
-	case '\r':
+		return Whitespace, "\n", nil
+
+	case '\r' == r:
 		t.Scanner.Next()
 		n := t.Scanner.Peek()
 		if n == '\n' {
 			t.Scanner.Next()
 		}
-		return Whitespace, "\r", nil
+		t.Line += 1
+		t.Col = 1
+		return Whitespace, "\n", nil
+
+	case 'N' == r:
+		t.Scanner.Next()
+		n := t.Scanner.Peek()
+		if n == '\'' {
+			str := tokenizeSingleQuotedString(t.Scanner)
+			t.Col += 3 + len(str)
+			return NationalStringLiteral, str, nil
+		}
+		s := tokenizeWord('N', t.Dialect, t.Scanner)
+		t.Col += len(s)
+		v := MakeKeyword(s, 0)
+		return SQLKeyword, v, nil
+
+	case t.Dialect.IsIdentifierStart(r):
+		t.Scanner.Next()
+		s := tokenizeWord(r, t.Dialect, t.Scanner)
+		t.Col += len(s)
+		return SQLKeyword, MakeKeyword(s, 0), nil
+
+	case '\'' == r:
+		s := tokenizeSingleQuotedString(t.Scanner)
+		t.Col += 2 + len(s)
+		return SingleQuotedString, s, nil
+
+	case t.Dialect.IsDelimitedIdentifierStart(r):
+		t.Scanner.Next()
+		end := matchingEndQuote(r)
+
+		var s []rune
+		for {
+			n := t.Scanner.Next()
+			if n == end {
+				break
+			}
+			s = append(s, n)
+		}
+		t.Col += 2 + len(s)
+
+		return SQLKeyword, MakeKeyword(string(s), r), nil
+
+	case '0' <= r && r <= '9':
+		var s []rune
+		for {
+			n := t.Scanner.Peek()
+			if ('0' <= n && n <= '9') || n == '.' {
+				s = append(s, n)
+				t.Scanner.Next()
+			} else {
+				break
+			}
+		}
+		t.Col = len(s)
+		return Number, string(s), nil
+
+	case '(' == r:
+		t.Scanner.Next()
+		t.Col += 1
+		return LParen, "(", nil
+
+	case ')' == r:
+		t.Scanner.Next()
+		t.Col += 1
+		return RParen, ")", nil
+
+	case ',' == r:
+		t.Scanner.Next()
+		t.Col += 1
+		return Comma, ",", nil
+
+	case '-' == r:
+		t.Scanner.Next()
+
+		if '-' == t.Scanner.Peek() {
+			t.Scanner.Next()
+
+			var s []rune
+			for {
+				ch := t.Scanner.Next()
+				if ch != scanner.EOF && ch != '\n' {
+					s = append(s, ch)
+				} else {
+					s = append(s, '\n')
+					return Whitespace, string(s), nil // Comment Node
+				}
+			}
+		}
+		return Minus, "-", nil
+
+	case '/' == r:
+		t.Scanner.Next()
+
+		if '*' == t.Scanner.Peek() {
+			t.Scanner.Next()
+			return Whitespace, tokenizeMultilineComment(t.Scanner), nil
+		}
+
+		return Div, "/", nil
+
+	case '+' == r:
+		t.Scanner.Next()
+		t.Col += 1
+		return Plus, "+", nil
+	case '*' == r:
+		t.Scanner.Next()
+		t.Col += 1
+		return Mult, "*", nil
+	case '%' == r:
+		t.Scanner.Next()
+		t.Col += 1
+		return Mod, "%", nil
+	case '=' == r:
+		t.Scanner.Next()
+		t.Col += 1
+		return Eq, "=", nil
+	case '.' == r:
+		t.Scanner.Next()
+		t.Col += 1
+		return Period, ".", nil
+
+	case '!' == r:
+		t.Scanner.Next()
+		n := t.Scanner.Peek()
+		if n == '=' {
+			t.Scanner.Next()
+			t.Col += 2
+			return Neq, "!=", nil
+		}
+		return ILLEGAL, "", errors.Errorf("tokenizer error: illegal sequence %s%s", string(r), string(n))
+
+	case '<' == r:
+		t.Scanner.Next()
+		switch t.Scanner.Peek() {
+		case '=':
+			t.Scanner.Next()
+			t.Col += 2
+			return LtEq, "<=", nil
+		case '>':
+			t.Scanner.Next()
+			t.Col += 2
+			return Neq, "<>", nil
+		default:
+			t.Col += 1
+			return Lt, "<", nil
+		}
+	case '>' == r:
+		t.Scanner.Next()
+		switch t.Scanner.Peek() {
+		case '=':
+			t.Scanner.Next()
+			t.Col += 2
+			return GtEq, ">=", nil
+		default:
+			t.Col += 1
+			return Gt, ">", nil
+		}
+	case ':' == r:
+		t.Scanner.Next()
+		n := t.Scanner.Peek()
+		if n == ':' {
+			t.Scanner.Next()
+			t.Col += 2
+			return DoubleColon, "::", nil
+		}
+		t.Col += 1
+		return Colon, ":", nil
+	case ';' == r:
+		t.Scanner.Next()
+		t.Col += 1
+		return Semicolon, ";", nil
+	case '\\' == r:
+		t.Scanner.Next()
+		t.Col += 1
+		return Backslash, "\\", nil
+	case '[' == r:
+		t.Scanner.Next()
+		t.Col += 1
+		return LBracket, "[", nil
+	case ']' == r:
+		t.Scanner.Next()
+		t.Col += 1
+		return RBracket, "]", nil
+	case '&' == r:
+		t.Scanner.Next()
+		t.Col += 1
+		return Ampersand, "&", nil
+	case '{' == r:
+		t.Scanner.Next()
+		t.Col += 1
+		return LBrace, "{", nil
+	case '}' == r:
+		t.Scanner.Next()
+		t.Col += 1
+		return RBrace, "}", nil
+	case scanner.EOF == r:
+		return ILLEGAL, "", io.EOF
+	default:
+		t.Scanner.Next()
+		t.Col += 1
+		return Char, string(r), nil
+	}
+}
+
+func tokenizeWord(f rune, dialect dialect.Dialect, s *scanner.Scanner) string {
+	var str []rune
+	str = append(str, f)
+
+	for {
+		r := s.Peek()
+		if dialect.IsIdentifierPart(r) {
+			s.Next()
+			str = append(str, r)
+		} else {
+			break
+		}
 	}
 
-	return ILLEGAL, "", nil
+	return string(str)
+}
+
+func tokenizeSingleQuotedString(s *scanner.Scanner) string {
+	var str []rune
+	s.Next()
+
+	for {
+		n := s.Peek()
+		if n == '\'' {
+			s.Next()
+			if s.Peek() == '\'' {
+				str = append(str, '\'')
+				s.Next()
+			} else {
+				break
+			}
+			continue
+		}
+
+		s.Next()
+		str = append(str, n)
+	}
+
+	return string(str)
+}
+
+func tokenizeMultilineComment(s *scanner.Scanner) string {
+	var str []rune
+	var mayBeClosingComment bool
+	for {
+		n := s.Next()
+
+		if mayBeClosingComment {
+			if n == '/' {
+				break
+			} else {
+				str = append(str, n)
+			}
+		}
+		mayBeClosingComment = n == '*'
+		if !mayBeClosingComment {
+			str = append(str, n)
+		}
+	}
+
+	return string(str)
 }
--- tokenizer_test.go
+++ tokenizer_test.go
@@ -8,39 +8,369 @@ import (
 	"github.com/akito0107/xsqlparser/dialect"
 )
 
-func TestTokenizer_NextToken(t *testing.T) {
-	t.Run("single character", func(t *testing.T) {
-
-		cases := []struct {
-			name string
-			in   string
-			out  *TokenSet
-		}{
-			{
-				name: "tokenize whitespace",
-				in:   " ",
-				out: &TokenSet{
+func TestTokenizer_Tokenize(t *testing.T) {
+	cases := []struct {
+		name string
+		in   string
+		out  []*TokenSet
+	}{
+		{
+			name: "whitespace",
+			in:   " ",
+			out: []*TokenSet{
+				{
 					Tok:   Whitespace,
 					Value: " ",
 				},
 			},
-		}
+		},
+		{
+			name: "whitespace and new line",
+			in: `
+ `,
+			out: []*TokenSet{
+				{
+					Tok:   Whitespace,
+					Value: "\n",
+				},
+				{
+					Tok:   Whitespace,
+					Value: " ",
+				},
+			},
+		},
+		{
+			name: "whitespace and tab",
+			in: "\r\n	",
+			out: []*TokenSet{
+				{
+					Tok:   Whitespace,
+					Value: "\n",
+				},
+				{
+					Tok:   Whitespace,
+					Value: "\t",
+				},
+			},
+		},
+		{
+			name: "N string",
+			in:   "N'string'",
+			out: []*TokenSet{
+				{
+					Tok:   NationalStringLiteral,
+					Value: "string",
+				},
+			},
+		},
+		{
+			name: "N string with keyword",
+			in:   "N'string' NOT",
+			out: []*TokenSet{
+				{
+					Tok:   NationalStringLiteral,
+					Value: "string",
+				},
+				{
+					Tok:   Whitespace,
+					Value: " ",
+				},
+				{
+					Tok: SQLKeyword,
+					Value: &SQLWord{
+						Value:   "NOT",
+						Keyword: "NOT",
+					},
+				},
+			},
+		},
+		{
+			name: "Ident",
+			in:   "select",
+			out: []*TokenSet{
+				{
+					Tok: SQLKeyword,
+					Value: &SQLWord{
+						Value:   "select",
+						Keyword: "SELECT",
+					},
+				},
+			},
+		},
+		{
+			name: "single quote string",
+			in:   "'test'",
+			out: []*TokenSet{
+				{
+					Tok:   SingleQuotedString,
+					Value: "test",
+				},
+			},
+		},
+		{
+			name: "quoted string",
+			in:   "\"SELECT\"",
+			out: []*TokenSet{
+				{
+					Tok: SQLKeyword,
+					Value: &SQLWord{
+						Value:      "SELECT",
+						Keyword:    "SELECT",
+						QuoteStyle: '"',
+					},
+				},
+			},
+		},
+		{
+			name: "parents with number",
+			in:   "(123),",
+			out: []*TokenSet{
+				{
+					Tok:   LParen,
+					Value: "(",
+				},
+				{
+					Tok:   Number,
+					Value: "123",
+				},
+				{
+					Tok:   RParen,
+					Value: ")",
+				},
+				{
+					Tok:   Comma,
+					Value: ",",
+				},
+			},
+		},
+		{
+			name: "minus comment",
+			in:   "-- test",
+			out: []*TokenSet{
+				{
+					Tok:   Whitespace,
+					Value: " test\n",
+				},
+			},
+		},
+		{
+			name: "minus operator",
+			in:   "1-3",
+			out: []*TokenSet{
+				{
+					Tok:   Number,
+					Value: "1",
+				},
+				{
+					Tok:   Minus,
+					Value: "-",
+				},
+				{
+					Tok:   Number,
+					Value: "3",
+				},
+			},
+		},
+		{
+			name: "/* comment",
+			in: `/* test
+multiline
+comment */`,
+			out: []*TokenSet{
+				{
+					Tok:   Whitespace,
+					Value: " test\nmultiline\ncomment ",
+				},
+			},
+		},
+		{
+			name: "operators",
+			in:   "1/1*1+1%1=1.1-.",
+			out: []*TokenSet{
+				{
+					Tok:   Number,
+					Value: "1",
+				},
+				{
+					Tok:   Div,
+					Value: "/",
+				},
+				{
+					Tok:   Number,
+					Value: "1",
+				},
+				{
+					Tok:   Mult,
+					Value: "*",
+				},
+				{
+					Tok:   Number,
+					Value: "1",
+				},
+				{
+					Tok:   Plus,
+					Value: "+",
+				},
+				{
+					Tok:   Number,
+					Value: "1",
+				},
+				{
+					Tok:   Mod,
+					Value: "%",
+				},
+				{
+					Tok:   Number,
+					Value: "1",
+				},
+				{
+					Tok:   Eq,
+					Value: "=",
+				},
+				{
+					Tok:   Number,
+					Value: "1.1",
+				},
+				{
+					Tok:   Minus,
+					Value: "-",
+				},
+				{
+					Tok:   Period,
+					Value: ".",
+				},
+			},
+		},
+		{
+			name: "Neq",
+			in:   "1!=2",
+			out: []*TokenSet{
+				{
+					Tok:   Number,
+					Value: "1",
+				},
+				{
+					Tok:   Neq,
+					Value: "!=",
+				},
+				{
+					Tok:   Number,
+					Value: "2",
+				},
+			},
+		},
+		{
+			name: "Lts",
+			in:   "<<=<>",
+			out: []*TokenSet{
+				{
+					Tok:   Lt,
+					Value: "<",
+				},
+				{
+					Tok:   LtEq,
+					Value: "<=",
+				},
+				{
+					Tok:   Neq,
+					Value: "<>",
+				},
+			},
+		},
+		{
+			name: "Gts",
+			in:   ">>=",
+			out: []*TokenSet{
+				{
+					Tok:   Gt,
+					Value: ">",
+				},
+				{
+					Tok:   GtEq,
+					Value: ">=",
+				},
+			},
+		},
+		{
+			name: "colons",
+			in:   ":1::1;",
+			out: []*TokenSet{
+				{
+					Tok:   Colon,
+					Value: ":",
+				},
+				{
+					Tok:   Number,
+					Value: "1",
+				},
+				{
+					Tok:   DoubleColon,
+					Value: "::",
+				},
+				{
+					Tok:   Number,
+					Value: "1",
+				},
+				{
+					Tok:   Semicolon,
+					Value: ";",
+				},
+			},
+		},
+		{
+			name: "others",
+			in:   "\\[{&}]",
+			out: []*TokenSet{
+				{
+					Tok:   Backslash,
+					Value: "\\",
+				},
+				{
+					Tok:   LBracket,
+					Value: "[",
+				},
+				{
+					Tok:   LBrace,
+					Value: "{",
+				},
+				{
+					Tok:   Ampersand,
+					Value: "&",
+				},
+				{
+					Tok:   RBrace,
+					Value: "}",
+				},
+				{
+					Tok:   RBracket,
+					Value: "]",
+				},
+			},
+		},
+	}
 
-		for _, c := range cases {
-			t.Run(c.name, func(t *testing.T) {
-				src := strings.NewReader(c.in)
-				tokenizer := Init(src, &dialect.GenericDialect{})
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			src := strings.NewReader(c.in)
+			tokenizer := NewTokenizer(src, &dialect.GenericSQLDialect{})
 
-				tok, err := tokenizer.NextToken()
-				if err != nil {
-					t.Errorf("should be no error %v", err)
+			tok, err := tokenizer.Tokenize()
+			if err != nil {
+				t.Errorf("should be no error %v", err)
+			}
+
+			if len(tok) != len(c.out) {
+				t.Fatalf("should be same length but %d, %d", len(tok), len(c.out))
+			}
+
+			for i := 0; i < len(tok); i++ {
+				if tok[i].Tok != c.out[i].Tok {
+					t.Errorf("%d, expected token: %d, but got %d", i, c.out[i].Tok, tok[i].Tok)
 				}
-
-				if !reflect.DeepEqual(tok.Tok, c.out.Tok) || !reflect.DeepEqual(tok.Value, c.out.Value) {
-					t.Errorf("expected: %+v, but got %+v", c.out, tok)
+				if !reflect.DeepEqual(tok[i].Value, c.out[i].Value) {
+					t.Errorf("%d, expected value: %+v, but got %+v", i, c.out[i].Value, tok[i].Value)
 				}
-
-			})
-		}
-	})
+			}
+		})
+	}
 }
