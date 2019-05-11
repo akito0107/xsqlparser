d279a4a3f2614e524ad5c2de1a4d7c0a34628d7a
Akito Ito <akito0107@gmail.com>
2019-05-11 16:07:26 +0900

    refactor: move to token

--- Makefile
+++ Makefile
@@ -1,9 +1,19 @@
+SHELL := PATH="$(PWD)/tools/bin:$(PATH)" $(SHELL)
+
 .PHONY: build
 build: bin/astprinter
 
 .PHONY: bin/astprinter
-bin/astprinter: vendor
+bin/astprinter: vendor generate
 	go build -o bin/astprinter cmd/astprinter/main.go
+
+.PHONY: tools/bin/genmark
+tools/bin/genmark:
+	go build -o tools/bin/genmark tools/genmark/main.go
+
+.PHONY: generate
+generate: tools/bin/genmark
+	go generate ./...
 
 vendor: Gopkg.toml Gopkg.lock
 	dep ensure
---
+++ token.go
@@ -0,0 +1,69 @@
+package xsqlparser
+
+type Token int
+
+//go:generate stringer -type Token token.go
+const (
+	// A keyword (like SELECT)
+	SQLKeyword Token = iota
+	// Numeric literal
+	Number
+	// A character that cloud not be tokenized
+	Char
+	// Single quoted string i.e: 'string'
+	SingleQuotedString
+	// National string i.e: N'string'
+	NationalStringLiteral
+	// Comma
+	Comma
+	// Whitespace
+	Whitespace
+	// = operator
+	Eq
+	// != or <> operator
+	Neq
+	// <  operator
+	Lt
+	// > operator
+	Gt
+	// <= operator
+	LtEq
+	// >= operator
+	GtEq
+	// + operator
+	Plus
+	// - operator
+	Minus
+	// * operator
+	Mult
+	// / operator
+	Div
+	// % operator
+	Mod
+	// Left parenthesis `(`
+	LParen
+	// Right parenthesis `)`
+	RParen
+	// Period
+	Period
+	// Colon
+	Colon
+	// DoubleColon
+	DoubleColon
+	// Semicolon
+	Semicolon
+	// Backslash
+	Backslash
+	// Left bracket `]`
+	LBracket
+	// Right bracket `[`
+	RBracket
+	// &
+	Ampersand
+	// Left brace `{`
+	LBrace
+	// Right brace `}`
+	RBrace
+	// ILLEGAL token
+	ILLEGAL
+)
